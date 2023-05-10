932dd10eaad21fcf7b784e014abc0f309d7243e1
PiroAvni <avnipiro.dev@gmail.com>
2023-05-10 10:36:54 +0100

    debug registry.js to include isAdmin & isBusiness

--- src/js/views/pages/register.js
+++ src/js/views/pages/register.js
@@ -1,45 +1,46 @@
-document.getElementById("register-form").addEventListener("submit", async (e) => {
+document
+  .getElementById("register-form")
+  .addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const form = new FormData(e.target);
-
-    
-
-
 
     // const isBusinessResult = form.get('isBusiness') === null ? 0 : 1
     // console.log(isAdminResult, );
     // console.log(isBusinessResult, );
 
-    const userType = document.querySelector('input[name ="radio-btn"]:checked').value
-    console.log('register',userType)
-//   const isBusiness = userType === 
+    const userType = document.querySelector(
+      'input[name ="radio-btn"]:checked'
+    ).id;
+    console.log("register", userType);
+    const isBusinessResult = userType === "isBusiness" ? 1 : 0;
+    const isAdminResult = userType === "isAdmin" ? 1 : 0;
 
+    console.log(isAdminResult);
+    console.log(isBusinessResult);
 
-   
     const options = {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: form.get("username"),
-            password: form.get("password"),
-            // isAdmin: isAdminResult,
-            // isBusiness: isBusinessResult,
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: form.get("username"),
+        password: form.get("password"),
+        isAdmin: isAdminResult,
+        isBusiness: isBusinessResult,
+      }),
+    };
 
-        })
-    }
-
-    const response = await fetch("https://florin-server-web.onrender.com/users/register", options);
+    const response = await fetch(
+      "https://florin-server-web.onrender.com/users/register",
+      options
+    );
     const data = await response.json();
-
-    if (response.status !== 201) {
-        // window.location.assign("login.html");
-
-    } else {
-        alert(data.error);
+    try {
+      window.location.assign("login.html");
+    } catch (error) {
+      console.log(error.message);
     }
-})
-
+  });
