e027952bb930431f713675a8db6b7d5b4616bf7f
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-12 10:13:23 +0100

    add more tests for board.spec.js

--- __tests__/board.spec.js
+++ __tests__/board.spec.js
@@ -37,4 +37,53 @@ describe('boardcopy.html', () => {
     const btn = document.getElementById("log-out")
     expect(btn).toBeTruthy()
   })
+
+  test("Username is displayed?", () => {
+    const username = document.getElementById("username-display")
+    expect(username).toBeTruthy()
+  })
+
+  test("Is there a h1?", () => {
+    const h1 = document.querySelector("h1")
+    expect(h1).toBeTruthy()
+  })
+
+  test("Does the h1 contain text?", () => {
+    const h1 = document.querySelector("h1").textContent
+    // const h1Length = h1.innerHTML
+    let containsText = h1 === "" ? false : true;
+    // if (h1 === ""){
+    //   containsText = false
+    // } else {
+    //   containsText = true
+    // }
+    expect(containsText).toBe(true)
+  })
+
+  test("Is there a form for adding details to a new post?", () => {
+    const form = document.getElementById("form")
+    expect(form).toBeTruthy()
+  })
+
+  test("Is there a place to add a title?", () => {
+    const title = document.getElementById("title")
+    expect(title).toBeTruthy()
+
+  })
+
+  test("Is the title input type text?", () => {
+    const busElement = document.getElementById('title')
+    let isRadio
+    if (busElement.type ==="text"){
+        isRadio = true
+    } else {
+        isRadio = false
+    }
+    expect(isRadio).toBe(true)
+  })
+  
+  test("Is there a place to add category?", () => {
+    const datalist = document.getElementById("DataList")
+    expect(datalist).toBeTruthy()
+  })
 })
